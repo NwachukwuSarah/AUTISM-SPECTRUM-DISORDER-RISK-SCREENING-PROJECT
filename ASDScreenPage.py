014c3de08ba4faef07237275efb2fdecffa72889
Nwachukwu  Sarah Ifunanya <137932423+NwachukwuSarah@users.noreply.github.com>
2026-02-01 18:00:37 -0800

    Add files via upload

---
+++ ASDScreenPage.py
@@ -0,0 +1,98 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+
+def prediction(A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, Age, Sex, Jaundice, Family_ASD):
+  data = {
+      "A1" : A1,
+      "A2" : A2,
+      "A3" : A3,
+      "A4" : A4,
+      "A5" : A5,
+      "A6" : A6,
+      "A7" : A7,
+      "A8" : A8,
+      "A9" : A9,
+      "A10" : A10,
+      "Age" : Age,
+      "Sex" : Sex,
+      "Jaundice" : Jaundice,
+      "Family_ASD" : Family_ASD
+  }
+
+  predictdata = pd.DataFrame([data])
+
+  predictdata["Sex"] = predictdata["Sex"].apply(lambda x: 'm' if x == "Male" else 'f')
+  predictdata["Jaundice"] = predictdata["Jaundice"].str.lower()
+  predictdata["Family_ASD"] = predictdata["Family_ASD"].str.lower()
+
+  with open("ASDmodel.pkl", "rb") as f:
+        bundle = pickle.load(f)
+
+  preprocesseddata = bundle["preprocess"](predictdata)
+  y_prob = bundle["ASDmodel"].predict(preprocesseddata)
+  Class = (y_prob >= 0.5).astype(int)
+
+  if Class[0] == 0:
+    st.write("The person is not at risk of having (Autism Spectrum Disorder) ASD")
+  else:
+    st.write("The person is at risk of having (Autism Spectrum Disorder) ASD")
+
+
+st.title("AUTISM RISK SCREENING")
+st.write("Choose a suitable answer from the options for the questions asked to predict whether a person is at the risk of having Autism Spectrum Disorder (ASD)")
+st.write("It is suitable for people of age 4 upward with suspected autism who do not have a learning disability")
+
+Sex = st.selectbox('If you are a parent/guardian answering for a child or adolescent, please select the child’s gender. If you are an adult participant, select your own gender', ['Male', 'Female'])
+Jaundice = st.selectbox('Have you/the Child ever had Jauundice before?', ['Yes', 'No'])
+Family_ASD = st.selectbox('Is there a family history of Autism Spectrum Disorder?', ['Yes', 'No'])
+Age = st.selectbox('What age group are you in? Note: 4-11(Child), 12-17(Adolescent), 18+(Adult)', ['Child', 'Adolescent', 'Adult'])
+
+#Ask ASD questions for each age group.
+options = ['Definitely Disagree', 'Slightly Disagree', 'Slightly Agree', 'Definitely Agree'] 
+
+A1 = A2 = A3 = A4 = A5 = A6 = A7 = A8 = A9 = A10 = None
+
+if Age == 'Child':
+  A1 = st.selectbox('She/he often notices small sounds when others do not ', options)
+  A2 = st.selectbox('She/he usually concentrates more on the whole picture, rather than the small details', options)
+  A3 = st.selectbox('In a social group, she/he can easily keep track of several different people’s conversations', options)
+  A4 = st.selectbox('She/he finds it easy to go back and forth between different activities', options)
+  A5 = st.selectbox('She/he doesn’t know how to keep a conversation going with his/her peers ', options)
+  A6 = st.selectbox('She/he is good at social chit-chat', options)
+  A7 = st.selectbox('When she/he is read a story, she/he finds it difficult to work out the character’s intentions or feelings', options)
+  A8 = st.selectbox('When she/he was in preschool, she/he used to enjoy playing games involving pretending with other children', options)
+  A9 = st.selectbox('She/he finds it easy to work out what someone is thinking or feeling just by looking at their face', options)
+  A10 = st.selectbox('She/he finds it hard to make new friends', options)
+
+elif Age == 'Adolescent':
+  A1 = st.selectbox('She/he notices patterns in things all the time', options)
+  A2 = st.selectbox('She/he usually concentrates more on the whole picture, rather than the small details', options)
+  A3 = st.selectbox('In a social group, she/he can easily keep track of several different people’s conversations', options)
+  A4 = st.selectbox('If there is an interruption, she/he can switch back to what she/he was doing very quickly', options)
+  A5 = st.selectbox('She/he frequently finds that she/he doesn’t know how to keep a conversation going', options)                               
+  A6 = st.selectbox('She/he is good at social chit-chat', options)
+  A7 = st.selectbox('When she/he was younger, she/he used to enjoy playing games involving pretending with other children', options)                                        
+  A8 = st.selectbox('She/he finds it difficult to imagine what it would be like to be someone else', options)                 
+  A9 = st.selectbox('She/he finds social situations easy', options)
+  A10 = st.selectbox('She/he finds it hard to make new friends', options)
+
+elif Age == 'Adult':
+  A1 = st.selectbox('I often notice small sounds when others do not', options)                                         
+  A2 = st.selectbox('I usually concentrate more on the whole picture, rather than the small details', options)
+  A3 = st.selectbox('I find it easy to do more than one thing at once', options)                                       
+  A4 = st.selectbox('If there is an interruption, I can switch back to what I was doing very quickly', options)
+  A5 = st.selectbox('I find it easy to ‘read between the lines’ when someone is talking to me', options)                                                                
+  A6 = st.selectbox('I know how to tell if someone listening to me is getting bored', options)
+  A7 = st.selectbox('When I’m reading a story I find it difficult to work out the characters’ intentions', options)                                                                            
+  A8 = st.selectbox('I like to collect information about categories of things (e.g. types of car, types of bird, types of train, types of plant etc) ', options)      
+  A9 = st.selectbox('I find it easy to work out what someone is thinking or feeling just by looking at their face', options)
+  A10 = st.selectbox('I find it difficult to work out people’s intentions', options)
+
+                                          
+if st.button('SCREEN AUTISM RISK'):
+    if all(v is not None for v in [A1, A2, A3, A4, A5, A6, A7, A8, A9, A10]):
+        prediction(A1, A2, A3, A4, A5, A6, A7, A8, A9, A10, Age, Sex, Jaundice, Family_ASD)
+    else:
+        st.error("Please answer all questions before screening")
